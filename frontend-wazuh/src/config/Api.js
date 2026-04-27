9da6bf65e21013b71f2c4980cae348a6a6f22742
Yosua <yosuakevan@gmail.com>
2026-04-27 15:15:23 +0700

    hapus base api

--- frontend-wazuh/src/config/Api.js
+++ frontend-wazuh/src/config/Api.js
@@ -4,7 +4,7 @@ const HOST = import.meta.env.VITE_HOST;
 const API_PORT = import.meta.env.VITE_API_PORT;
 const SOCKET_PORT = import.meta.env.VITE_SOCKET_PORT;
 
-export const API_BASE_URL = "http://127.0.0.1:5000";
+export const API_BASE_URL = "";
 export const SOCKET_URL = `http://${HOST}:${SOCKET_PORT}`;
 
 // Create Axios instance with default config
